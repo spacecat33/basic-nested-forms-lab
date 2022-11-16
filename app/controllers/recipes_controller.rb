6d1a930be6bc7bb3ec0b555aad38a5c3c7e4f5a6
Janan Akkad <jananakkad@hotmail.co.uk>
2022-11-16 18:42:01 +0000

    commit tree 8728aa677e20741403f68fab580675e003bdc406

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,28 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'ingredient_1_name', quantity: 'ingredient_1_quantity')
+    @recipe.ingredients.build(name: 'ingredient_2_name', quantity: 'ingredient_2_quantity')
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe) #or redirect to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+    :ingredients_attributes => [
+      :name,
+      :quantity
+    ]
+  )
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,22 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  # accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients_attributes) #Magic of nested forms
+    # the shortcut would expect:
+    #  ingredients_attributes = [
+    #   {:ingredient_1_name => "Ingredient 1 Name", :ingredient_1_quantity => "Quantity"},
+    #   {:ingredient_2_name => "Ingredient 2 Name", :ingredient_2_quantity => "Quantity"}
+    # ]
+
+    # but the full method typed up gets this - when used with fields_for rails will generae these:
+    # ingredients_attributes = {
+    #   0 => {:ingredient_1_name => "Ingredient 1 Name", :ingredient_1_quantity => "Quantity"},
+    #   1 => {:ingredient_2_name => "Ingredient 2 Name", :ingredient_2_quantity => "Quantity"}
+    # }
+    # raise ingredients_attributes.inspect
+    ingredients_attributes.each do |i, ingredient_attributes|    #i is the amount of attributes being made and the second one is ingredient attirbutes 
+        self.ingredients.build(ingredient_attributes)
+    end
+  end
 end
